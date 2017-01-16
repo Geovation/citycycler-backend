afc5c2cd49d1ab53da03e9050a959a834652883f
Sebastian Ovide <sebastian.ovide@gmail.com>
2017-01-16 14:17:16 +0000

    hardcoding endpoint (#9)

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,6 +1,7 @@
 import * as Koa from "koa";
 import * as bodyParser from "koa-bodyparser";
 import * as qs from "koa-qs";
+import * as Router from "koa-router";
 import * as cors from "koa2-cors";
 
 // local modules
@@ -10,6 +11,35 @@ export const app = new Koa();
 
 // enable qs for query string parsing
 qs(app, "strict");
+
+// TODO: to integrate with swagger (??)
+let hardCodedPhotos = [
+    "Pokémon Yellow",
+    "Super Metroid",
+    "Mega Man X",
+    "The Legend of Zelda",
+    "Pac-Man",
+    "Super Mario World",
+    "Street Fighter II",
+    "Half Life",
+    "Final Fantasy VII",
+    "Star Fox",
+    "Tetris",
+    "Donkey Kong III",
+    "GoldenEye 007",
+    "Doom",
+    "Fallout",
+    "GTA",
+    "Halo",
+];
+let router = new Router();
+router.get("/api/v0/photos",  (ctx, next) => {
+    ctx.body = hardCodedPhotos;
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
 
 app.use(cors({
   headers: ["Content-Type", "api_key", "Authorization"],
