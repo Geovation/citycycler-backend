a36094c423055a0b2eac94c7f7c84a1ab3b49dd0
Oliver Fawcett-Griffiths <olly@ollyfg.com>
2017-03-14 10:49:05 +0000

    Some more small changes so that serving locally works.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { start } from "./microservices-framework/app";
 
 // import * as Auth from "./common/auth";
-import * as Storage from "./common/storage";
+// import * as Storage from "./common/storage";
 
-// init some services services
+// init some services
 // Auth.init();
-Storage.init();
+// Storage.init();
 
 // start micro service framework
 start();
