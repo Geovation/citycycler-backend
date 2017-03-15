d7dcacaf543cbe628bc58b8154b5ffb60ca7eab9
Oliver Fawcett-Griffiths <olly@ollyfg.com>
2017-03-15 10:09:49 +0000

    Updated the e2e tests wth some other tests. They might not work - I'll look into them later.

--- src/app.e2e.ts
+++ src/app.e2e.ts
@@ -1,4 +1,4 @@
-import { config } from "./microservices-framework/config";
+// import { config } from "./microservices-framework/config";
 import { app, gracefulShutdown, setupServer } from "./microservices-framework/web/server";
 import * as chai from "chai";
 import * as EventEmitter from "events";
@@ -8,7 +8,7 @@ const expect = chai.expect;
 
 describe("MatchMyRoute API", () => {
     const startServer = !process.env.URL;
-    const url = process.env.URL || "http://localhost:8081";
+    // const url = process.env.URL || "http://localhost:8080";
     let server;
     let result = {
         body: null,
@@ -22,7 +22,7 @@ describe("MatchMyRoute API", () => {
         setupServer(appEmitter);
         appEmitter.on("ready", () => {
             if (startServer) {
-                server = app.listen(process.env.PORT || "8081", () => {
+                server = app.listen(process.env.PORT || "8080", () => {
                     console.log("App listening on port %s", server.address().port);
                     console.log("Press Ctrl+C to quit.");
                     finalDone();
@@ -34,23 +34,18 @@ describe("MatchMyRoute API", () => {
         });
 
         function finalDone() {
-            const username = "timepix";
-            const password = "TimepixRocks!";
-            const auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
             request({
                 headers: {
-                    Authorization: auth,
                     Origin: "https://www.example.com",
                 },
-                url: url,
-            }, (error, response, body) => {
-                console.log("error: ", error);
-                console.log("body: ", body);
-                result.error = error;
-                result.response = response;
-                result.body = error ? body : JSON.parse(body);
-                done();
-            });
+                url: "http://localhost:8080",
+            },
+                (error, response, body) => {
+                    result.error = error;
+                    result.response = response;
+                    result.body = body;
+                    done();
+                });
         }
     });
 
@@ -78,4 +73,37 @@ describe("MatchMyRoute API", () => {
         });
     });
 
+    describe("Routes", () => {
+        let validRouteId;
+        describe("Valid route creation", () => {
+            request({
+                headers: {
+                    Origin: "https://www.example.com",
+                },
+                json: {
+                    averageSpeed: 10,
+                    departureTime: 8500,
+                    owner: 1,
+                    route: [[0, 0], [1, 0], [1, 1]],
+                },
+                method: "POST",
+                url: "http://localhost:8080/route",
+            }, (error, response, body) => {
+                it("should return 200", (done) => {
+                    expect(response.statusCode).to.equal(200);
+                    done();
+                });
+                it("should return valid JSON", (done) => {
+                    expect(() => { JSON.parse(body); }).not.toThrow();
+                    done();
+                });
+                it("should return an integer Route ID", (done) => {
+                    expect(() => { parseInt(JSON.parse(body).result, 10); }).not.to.equal(NaN);
+                    done();
+                });
+
+                //validRouteId = parseInt(JSON.parse(body).result, 10);
+            });
+        });
+    });
 });
