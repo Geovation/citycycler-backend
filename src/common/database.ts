acfc91edbdd87c335096b18d3e4414672387d28b
Oliver Fawcett-Griffiths <olly@ollyfg.com>
2017-03-16 09:05:20 +0000

    Removed a bunch of old comments and made a change so that the DB name is now a environment variable.

--- src/common/database.ts
+++ src/common/database.ts
@@ -3,25 +3,6 @@ import { RouteDataModel } from "./RouteDataModel";
 import * as pg from "pg";
 import * as logger from "winston";
 
-// Datastore
-// type DatastoreKind = "Image" | "User";
-// const datastore = Datastore();
-// const queryImages = (query, queryName): Promise<ImageResultModel[]> => {
-//
-//     return datastore.runQuery(query)
-//         .then( result => {
-//             const images = result[0];
-//             const info = result[1];
-//             logger.debug("entities", JSON.stringify(images));
-//             logger.debug("info", JSON.stringify(info));
-//
-//             return images.map( (entity: ImageMetadataModel) => {
-//                 entity.id = entity[datastore.KEY].id;
-//                 return ImageResultModel.makeFromImageMetadata(entity);
-//             });
-//         });
-// };
-
 // create a config to configure both pooling behavior
 // and client options
 // note: all config is optional and the environment variables
@@ -29,7 +10,7 @@ import * as logger from "winston";
 const config = {
     database: "matchMyRoute", // env var: PGDATABASE
     // host: "35.190.143.196", // Server hosting the postgres database
-    host: "/cloudsql/" + process.env.DB_CONNECTION_NAME, // Server hosting the postgres database
+    host: process.env.DB_CONNECTION_PATH, // Server hosting the postgres database
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
     max: 10, // max number of clients in the pool
     port: 5432, // env var: PGPORT
@@ -181,63 +162,3 @@ export function getRoutesNearby(radius: number, lat: number, lon: number): Promi
 
     });
 }
-
-// export function saveImageMetadata(ownerId: string, imageMetadata: ImageMetadataModel): Promise<ImageResultModel> {
-//     const kindImage: DatastoreKind = "Image";
-//
-//     // As we are changing the DB, there is not point to fix this.
-//     // const kindUser: DatastoreKind = "User";
-//
-//     // const imageDSEntity = {
-//     //     data: imageMetadata,
-//     //     key: datastore.key(kindUser, ownerId, kindImage),
-//     // };
-//
-//     const imageDSEntity = {
-//         data: imageMetadata,
-//         key: datastore.key(kindImage),
-//     };
-//
-//     return datastore.save(imageDSEntity)
-//         .then( data => {
-//             const id = data[0].mutationResults[0].key.path[0].id;
-//             imageMetadata.id = id;
-//
-//             return ImageResultModel.makeFromImageMetadata(imageMetadata);
-//         });
-// }
-//
-// export function getUserById(id) {
-//     const kind: DatastoreKind = "User";
-//     const query = datastore.createQuery(kind)
-//         .filter("__key__", "=", datastore.key([kind, id]));
-//
-//     return datastore.runQuery(query)
-//         .then(result => {
-//             const users = result[0];
-//             const info = result[1];
-//             logger.debug("entities", JSON.stringify(users));
-//             logger.debug("info", JSON.stringify(info));
-//             return(_.extend({id}, users[0]));
-//         });
-// }
-//
-// export function getImages() {
-//     const kind: DatastoreKind = "Image";
-//     const query = datastore.createQuery(kind);
-//     return queryImages(query, "getImage");
-// }
-//
-// export function getImageById(id): Promise<any> {
-//     const kind: DatastoreKind = "Image";
-//     const query = datastore.createQuery(kind)
-//         .filter("__key__", "=", datastore.key([kind, id]));
-//     return queryImages(query, "getImageById")
-//         .then(images => images[0]);
-// }
-//
-// export function deleteImageMetadata(id) {
-//     const kind: DatastoreKind = "Image";
-//     const key = datastore.key([kind, id]);
-//     return datastore.delete(key);
-// }
