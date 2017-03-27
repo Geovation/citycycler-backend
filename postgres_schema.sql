248a8cd319c0fe5875d9708cfe7daa0ddcae8ef3
Oliver Fawcett-Griffiths <olly@ollyfg.com>
2017-03-27 14:10:46 +0100

    Added a file postgres_schema.sql with our db schema
    This file can be executed and will setup a database with all the
    tables and indexes that we need.

---
+++ postgres_schema.sql
@@ -0,0 +1,30 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE TABLE users (
+-- Core User attributes. Can't be null
+id serial PRIMARY KEY,
+name varchar(200) NOT NULL,
+email varchar(200) UNIQUE,
+pwh bytea NOT NULL,
+salt bytea NOT NULL,
+rounds integer NOT NULL,
+jwt_secret varchar(32) NOT NULL,
+-- User profile info. Should be optional
+profile_bio text,
+profile_photo varchar(200),
+profile_joined integer DEFAULT extract(epoch from now())::Integer,
+profile_help_count integer DEFAULT 0,
+profile_rating_sum integer DEFAULT 0    -- Average rating = rating_sum/help_count
+);
+
+CREATE TABLE routes (
+id serial PRIMARY KEY,
+route geography NOT NULL,		-- The route itself
+departureTime integer NOT NULL,	-- Seconds past midnight that the owner cycles this route
+arrivalTime integer NOT NULL,	-- The time that a user arrives in seconds past midnight
+days smallint NOT NULL,			-- A bitmask of the days of the week a user cycles this route
+owner integer REFERENCES users ON DELETE CASCADE	-- User who created this route
+);
+
+CREATE INDEX IF NOT EXISTS route_index ON routes USING GIST ( "route" );
+CREATE INDEX IF NOT EXISTS owner_index ON routes USING btree ( "owner" );
