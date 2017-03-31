37dfdcb3f1ef88e925dfdf1298661d8988ee7d9a
Oliver Fawcett-Griffiths <olly@ollyfg.com>
2017-03-31 12:30:58 +0100

    Add database credentials to the testing environment

--- gulpfile.js
+++ gulpfile.js
@@ -78,6 +78,8 @@ function getBaseEnvVars() {
         WITH_SERVICES: true,
         NODE_PATH: ".",
         PGDATABASE: "matchMyRoute",
+        PGUSER: "postgres",
+        PGPASSWORD: "aUZw[:Gw38H&>Jf2hUwd",
     };
 };
 
