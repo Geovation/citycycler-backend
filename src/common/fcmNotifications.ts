03f535455b12e7cb8f6d9ca422ff87cda8965af0
MStephan <max.stephan@geovation.uk>
2017-07-25 11:43:58 +0100

    notify experienced rider when he receives a new buddy request

---
+++ src/common/fcmNotifications.ts
@@ -0,0 +1,34 @@
+import * as firebaseAdmin from "firebase-admin";
+
+export function notify(
+    userId: number,
+    messagePayload: firebaseAdmin.messaging.MessagingPayload) {
+    console.log("user is notified");
+    getRegistrationTokensForUser(userId).then(
+        registrationTokens => {
+            // send notification
+            console.log("sending notification");
+            firebaseAdmin.messaging().sendToDevice(
+                registrationTokens,
+                messagePayload
+            );
+        }
+    );
+}
+
+function getRegistrationTokensForUser(userId: number) {
+    const database = firebaseAdmin.database();
+    const deviceTokenRef = database.ref("deviceTokens");
+    const userRef = deviceTokenRef.child(userId + "");
+
+    return userRef.once("value").then(
+        snapshot => {
+            let registrationTokens = [];
+            snapshot.forEach(child => {
+                registrationTokens.push(child.val());
+                console.log("Registration token found " + child.val());
+            });
+            return registrationTokens;
+        }
+    );
+}
